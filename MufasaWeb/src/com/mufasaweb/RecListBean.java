e742c28b66461d6b178a6026bb8a7b3071a100af
Wei Wang <ww85@uw.edu>
2015-09-15 20:14:50 -0700

    process user input for recommendation results

---
+++ MufasaWeb/src/com/mufasaweb/RecListBean.java
@@ -0,0 +1,18 @@
+package com.mufasaweb;
+
+import java.util.List;
+
+public class RecListBean {
+	
+	private List<String> beanRecList;
+	
+	
+	public void setRecList(List<String> r) {
+		beanRecList = r;
+	}
+	
+	public List<String> getRecList(){
+		return beanRecList;
+	}
+
+}
